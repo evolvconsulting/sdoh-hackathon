8170976b9cafe80353f2e57076a1d6209801091d
Mario Bolivar <mario.bolivar@evolvconsulting.com>
2024-03-15 18:10:10 -0500

    Added MudBlazor to Portal.Patient

--- src/SDOH.Hackathon/Portal.Patient/Program.cs
+++ src/SDOH.Hackathon/Portal.Patient/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace Portal.Patient
 {
@@ -10,6 +11,8 @@ namespace Portal.Patient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddMudServices();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
